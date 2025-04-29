99d44e58c29b597616045f8480c125718fdc42ec
고윤아 <0205ria@gmail.com>
2025-04-29 16:32:14 +0900

    ✨[Feat] Implement player movement
    * Added basic player movement with forward motion and flap control.
    * Implemented collision detection that triggers death state and cooldown.
    * Fixed typo (isDie -> isDead)

---
+++ FlappyPlane/Assets/Scripts/Player.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D _rigidbody;
+
+    public float flapForce = 6f;
+    public float forwardSpeed = 3f;
+    public bool isDead = false;
+    private float deathCooldown = 0f;
+
+    private bool isFlap = false;
+
+    public bool godMode = false;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        if (animator == null)
+            Debug.LogError("Not Founded Animator");
+
+        if (_rigidbody == null)
+            Debug.LogError("Not Founded rigidbody");
+    }
+
+
+    private void Update()
+    {
+        if (isDead)
+        {
+            if (deathCooldown <= 0f)
+            {
+                // 게임 재시작
+            }
+            else
+            {
+                deathCooldown -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                isFlap = true;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDead) return;
+        
+        Vector3 velocity = _rigidbody.velocity;
+        velocity.x = forwardSpeed; // 앞으로는 일정하게 이동
+
+        if (isFlap)
+        {
+            velocity.y += flapForce; // 클릭할 경우에만 위로 이동
+            isFlap = false; // 바로 false
+        }
+
+        _rigidbody.velocity = velocity;
+
+        float angle = Mathf.Clamp((_rigidbody.velocity.y * 10f), -90, 90);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (godMode) return;
+
+        if (isDead) return;
+
+        isDead = true;
+        deathCooldown = 1f;
+        animator.SetInteger("isDead", 1);
+    }
+}
